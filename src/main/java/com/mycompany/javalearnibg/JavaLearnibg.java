20cd237f56c5d8fd6806719373c7c749e544d53a
Fernand OUEDRAOGO <fernandouedraogo96@gmail.com>
2024-04-29 12:18:21 +0000

    Initialize the project

---
+++ src/main/java/com/mycompany/javalearnibg/JavaLearnibg.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javalearnibg;
+
+/**
+ *
+ * @author fernand
+ */
+public class JavaLearnibg {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
